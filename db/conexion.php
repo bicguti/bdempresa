6cd1167e68e7b9ebb32b93f13b0ddf3c76e62a78
Byron Castro <bicguti@gmail.com>
2018-03-17 11:32:31 -0600

    Archivo de conexion eliminado

--- db/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-   require "../vendor/autoload.php";
-   // connect to mongodb using localhost or ip adress
-   $m = new MongoDB\Client('mongodb://172.16.6.224:27017');
-   // select a database
-   $db = $m->proyecto;
-   // echo "Base de datos proyecto seleccionado!!!";
-?>
